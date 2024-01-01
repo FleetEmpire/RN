e921b20db47709cac8d1642f598b55c9564df484
FleetEmpire <star-space@outlook.com>
2024-01-01 21:06:23 +0800

    deleted:    RN_GUI.py 	modified:   RnCore.py 	new file:   app.py 	new file:   templates/index.html 	new file:   templates/results.html

--- RnCore.py
+++ RnCore.py
@@ -1,28 +1,30 @@
 import random
 
+
 class RandomNumberCore:
-    def __init__(self, start, stop):
+    def __init__(self, start: int, stop: int):
         self.start = start
         self.stop = stop
-        self.ranges = list(range(start, stop+1))
-        
+        self.ranges = list(range(start, stop + 1))
+
     def random_number_list_choice(self):
         return random.choice(self.ranges)
-    
-    def random_number_faster(self):
-        return random.randrange(self.start, self.stop+1)
-    
+
+    def random_number_fast(self):
+        return random.randrange(self.start, self.stop + 1)
+
+
 class RandomNumber_except(RandomNumberCore):
     def __init__(self, start, stop):
         super().__init__(start=start, stop=stop)
-        
+
     def random_number_except(self, *except_numbers):
         for n in except_numbers:
             self.ranges.remove(n)
         return self.random_number_list_choice()
-    
+
+
 if __name__ == '__main__':
     for i in range(1, 52):
         N = RandomNumber_except(1, 51)
         print(N.random_number_except(15, 16, 41))
-        
---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+import RnCore as rc
+
+rn = Flask(__name__)
+
+def get_results(start, end):
+    RCore = rc.RandomNumberCore(start, end)
+    results = RCore.random_number_fast()
+    return results
+
+@rn.route("/", methods=["GET", "POST"])
+def index():
+    
+    """
+    The index Page method
+
+    Returns:
+        Response
+    """
+
+    if request.method == "POST":
+        global results, start1, end1
+        start = request.form['start']
+        end = request.form['end']
+        
+        try:
+            start1 = int(str(start))
+            end1 = int(str(end))
+            
+        except ValueError:
+            flash("Invalid input.")
+            return redirect(url_for("index"))
+        
+        else:
+            results = get_results(start=start1, end=end1)
+            return redirect(url_for("result"))
+          
+    return render_template("index.html")
+
+
+@rn.route("/results", methods=["GET", "POST"])
+def result():
+
+    """
+        The result Page
+
+        Returns:
+            Response
+    """
+    if request.method == "POST":
+        global results
+        results = get_results(start=start1, end=end1)
+        return redirect(url_for("result"))
+    
+    return render_template("results.html", results = results)
+
+@rn.route("/fuck")
+def fuck():
+    return '<h1>傻逼政治！</h1>'
